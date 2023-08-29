309a8778e1fefdf23bdd91a224481a1e91b555ee
Jacob Lang <jacoblang72@comcast.net>
2023-08-29 16:42:56 -0400

    morestyleds

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ import { StyledHome } from "./styledComps"
 export default function Home(props) {
     return (
         <StyledHome>
+            <div>
             <div>H</div>
             <div>o</div>
             <div>m</div>
             <div>e</div>
+            </div>
+            <input value = "Start Here" type = "button"/>
         </StyledHome>
     )
 }
--- src/components/styledComps.js
+++ src/components/styledComps.js
@@ -18,6 +18,28 @@ const kf = keyframes`
  color : rgb(39, 39, 39);
 }
 `
+const kf2 = keyframes`
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+`
+const activekf = keyframes`
+0% {
+    outline-offset : 8px;
+}
+25% {
+    outline-offset : 10px;
+}
+100% {
+    outline-offset : 14px;
+}
+`
 
 export const StyledHeader = styled.div`
 background-color : rgb(39, 39, 39);
@@ -35,9 +57,17 @@ a {
 export const StyledHome = styled.div`
 background-color : rgb(92, 92, 92);
 height : 100vh;
+background-image : linear-gradient(-45deg,rgb(39, 39, 39)
+,rgb(156, 153, 153),rgb(235, 232, 232),rgb(156, 153, 153) );
+background-size : 400% 400% ;
+animation : ${kf2} 15s infinite;
 display : flex;
 justify-content : center;
 align-items : center;
+flex-direction : column;
+div {
+    display : flex;
+    margin-bottom : 3rem;
 div {
     animation : ${kf} 5s infinite; 
 }
@@ -55,5 +85,26 @@ div:nth-of-type(3) {
 div:nth-of-type(4) {
     font-size : 50px;
     animation-delay : .3s;
+}
+}
+input {
+    width : 300px;
+    height : 75px;
+    border-radius : 24px;
+    border : none;
+    outline : 2px solid rgb(39, 39, 39);
+    background-color : white;
+    color : black;
+    font-size : 24px;
+    text-align : center;
+    &:hover {
+        background-color : rgb(39,39,39);
+        color : white;
+        outline-offset : 4px;
+        transition : .2s ease-in-out;
+    }
+    &:active {
+       animation : ${activekf} .1s ease-in-out forwards;
+    }
 }
 `
