fa0b3f1e826de1f3b74d40e494c4847b5bfdd6e2
Jacob Lang <jacoblang72@comcast.net>
2023-08-29 17:01:21 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Home from "./components/Home";
 import {Routes,Route,Link} from "react-router-dom"; 
 import FirstForm from "./components/First-Form/FirstForm";
 import {StyledHeader} from "./components/styledComps";
+import {useState} from "react"; 
+import { useVisible } from "./components/mainHooks/useVisible";
 
+const initial = false;
 export default function App() {
+  const [visible,setVisible] = useVisible("visible",initial)
   return (
     <div>
       <StyledHeader>
@@ -12,7 +16,8 @@ export default function App() {
       <Link to = "form-1">Form-1</Link>
       </StyledHeader>
       <Routes>
-        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/" 
+        element = {<Home visible = {visible} setVisible = {setVisible} />}/>
         <Route path = "form-1" element = {<FirstForm />} />
       </Routes>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,22 @@
-import { StyledHome } from "./styledComps"
+import { StyledHome } from "./styledComps";
+import { Routes, Route, Link } from "react-router-dom";
+
 
 export default function Home(props) {
     return (
         <StyledHome>
             <div>
-            <div>H</div>
-            <div>o</div>
-            <div>m</div>
-            <div>e</div>
+                <div>H</div>
+                <div>o</div>
+                <div>m</div>
+                <div>e</div>
             </div>
-            <input value = "Start Here" type = "button"/>
+            <input value="View Forms" type="button" onClick={props.setVisible} />
+            {props.visible &&
+                <div id="visible">
+                    <Link to="form-1">Form-1</Link>
+                </div>
+            }
         </StyledHome>
     )
 }
