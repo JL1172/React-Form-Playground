09b6c1064ddf003bf0bfcfb2034376dcf07444ef
Jacob Lang <jacoblang72@comcast.net>
2023-08-29 21:12:41 -0400

    wip

--- src/components/First-Form/ffhooks/useLocalStorage.js
+++ src/components/First-Form/ffhooks/useLocalStorage.js
@@ -3,17 +3,16 @@ import {useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 export const useLocalStorage = (key,initialValue) => {
-    const [data,setData] = useState(()=> {
+    const [visible,setVisible] = useState(()=> {
         if (window.localStorage.getItem(key)) {
             return JSON.parse(window.localStorage.getItem(key));
         } 
         window.localStorage.setItem(key,JSON.stringify(initialValue));
         return initialValue;
-    })
-    const changeData = value => {
-        changeData(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
-    }
-
-    return [data,setData]; 
+})
+const changeHandler = (value) => {
+    setVisible(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+return [visible,changeHandler];
 }
